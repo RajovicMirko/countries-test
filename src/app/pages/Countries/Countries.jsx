4c9cb35efa8059c715aa70b2038c2f19eb6a5ab8
unknown <rajovicmirko@gmail.com>
2021-01-14 16:11:20 +0100

    Changes Small bugfix Counties page shape definition

--- src/app/pages/Countries/Countries.jsx
+++ src/app/pages/Countries/Countries.jsx
@@ -1,21 +1,48 @@
+import "./Countries.scss";
 import React from "react";
 import InputText from "components/global/Input/Text";
+import Select from "components/global/Select";
 
 function Countries() {
-  const handleChange = (value) => {
+  const filterOptions = [
+    { key: "africa", value: "Africa" },
+    { key: "america", value: "America" },
+    { key: "asia", value: "Asia" },
+    { key: "europe", value: "Europe" },
+    { key: "oceania", value: "Oceania" },
+  ];
+
+  const handleSearch = (value) => {
     console.log("search value " + value);
   };
 
+  const handleFilter = (value) => {
+    console.log("filter value " + value);
+  };
+
   return (
-    <section className="counties-page">
+    <section className="countries">
       <section className="controles">
         <InputText
           placeholder="Search for a country..."
           icon="fas fa-search"
-          onChange={handleChange}
+          onChange={handleSearch}
+        />
+
+        <Select
+          defaultValue="Filter by Region"
+          options={filterOptions}
+          onChange={handleFilter}
         />
       </section>
-      <section className="grid"></section>
+      <section className="cards">
+        <div>cards</div>
+        <div>cards</div>
+        <div>cards</div>
+        <div>cards</div>
+        <div>cards</div>
+        <div>cards</div>
+      </section>
     </section>
   );
 }
---
+++ src/components/global/Select/Select.jsx
@@ -0,0 +1,34 @@
+import "./Select.scss";
+import React, { useState, useEffect } from "react";
+
+const defaultProps = {
+  options: [],
+  defaultText: "Select an option",
+  onChange: () => {},
+};
+
+function Select(props) {
+  const { options, value, onChange, defaultText } = props;
+  const [selectValue, setSelectValue] = useState(value);
+
+  useEffect(() => onChange(selectValue), [selectValue, onChange]);
+
+  const _handleChange = (event) => {
+    const value = event.target.value;
+    setSelectValue(value);
+  };
+
+  return (
+    <select className="select" onChange={_handleChange}>
+      <option value="">{defaultText}</option>
+      {options.map((option) => (
+        <option key={option.key} value={option.key}>
+          {option.value}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+Select.defaultProps = defaultProps;
+export default Select;
